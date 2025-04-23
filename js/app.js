a08a67bada81d66132cd0296c5ab9571de212a0a
ronaldarivargas <ronaldlimontco@gmail.com>
2025-04-22 23:17:49 -0500

    Modificación del archivo app.js

--- js/app.js
+++ js/app.js
@@ -1,54 +1,108 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const taskForm = document.getElementById('task-form');
+    //const taskForm = document.getElementById('task-form');
     const taskInput = document.getElementById('task-input');
     const taskList = document.getElementById('task-list');
 
     let tasks = [];
     let isEditing = false;
     let editingId = null;
-
-    taskForm.addEventListener('click', (e) => {
-        const vti = taskInput.value.trim();
-        if (vti !== '') {
-            if (isEditing) {
-                tasks = tasks.map(task =>
-                    task.id === editingId ? {
-                        ...task, text: vti
-                    } : task);
-                isEditing = false;
-                editingId = null;
-                taskForm.innerText = "Agregar";
+    /*
+        taskForm.addEventListener('click', (e) => {
+            const vti = taskInput.value.trim();
+            if (vti !== '') {
+                if (isEditing) {
+                    tasks = tasks.map(task =>
+                        task.id === editingId ? {
+                            ...task, text: vti
+                        } : task);
+                    isEditing = false;
+                    editingId = null;
+                    taskForm.innerText = "Agregar";
+                }
+                else {
+                    const task = {
+                        id: Date.now(),
+                        text: vti,
+                        complete: false
+                    };
+                     tasks.push(task);
+                    console.log(tasks);
+                }
+                renderTasks();
+                taskInput.value = '';
             }
-            else {
-                const task = {
-                    id: Date.now(),
-                    text: vti,
-                    complete: false
-                };
-                tasks.push(task);
-                console.log(tasks);
-            }
-            renderTasks();
-            taskInput.value = '';
-        }
-    });
-
+        });
+    */
     function renderTasks() {
+        console.log("Runing");
+        fetch('server/user/session_info.php')
+            .then(res => res.json())
+            .then(data => {
+                if (data.user_id) {
+                    console.log('Sesión activa para usuario:', data.user_id);
+                    fetch('server/task/index.php?user_id=' + data.user_id)
+                        .then(response => response.json())
+                        .then(tks => {
+                            console.log(tks);
+                            tks.forEach(
+                                task => {
+                                    console.log(task);
+
+                                    const li = document.createElement('li');
+                                    if (task.completed) {
+                                        li.className = 'task-ready';
+                                        li.innerHTML =
+                                            '<span>' + task.title + '</span>';
+                                    }
+                                    else {
+                                        li.innerHTML =
+                                            '<span>' + task.title + '</span>' +
+                                            '<div>' +
+                                            '<button class="complete-btn" onclick="completeTask(' + task.id + ')">' +
+                                            'Completar </button>' +
+                                            '<button class="edit-btn" onclick="editTask(' + task.id + ')">' +
+                                            'Editar </button>' +
+                                            '<button class="delete-btn" onclick="deleteTask(' + task.id + ')">' +
+                                            'Eliminar </button>' +
+                                            '</div>';
+                                    }
+                                    taskList.appendChild(li);
+                                }
+                            );
+                        });
+                } else {
+                    console.warn(data.error);
+                    window.location.href = 'login.php';
+                }
+            });
+
+
+
+
+
         taskList.innerHTML = '';
         tasks.forEach(
             task => {
                 console.log(task);
 
                 const li = document.createElement('li');
-                li.className='flex justify-between items-center bg-gray-100 px-4 py-2 rounded';
-                li.innerHTML =
-                    '<span>' + task.text + '</span>' +
-                    '<div class="space-x-2">' +
-                    '<button class="text-blue-600 hover:underline" onclick="editTask(' + task.id + ')">' +
-                    'Editar </button>' +
-                    '<button class="text-red-600 hover:underline" onclick="deleteTask(' + task.id + ')">' +
-                    'Eliminar </button>' +
-                    '</div>';
+                if (task.complete) {
+                    li.className = 'task-ready';
+                    li.innerHTML =
+                        '<span>' + task.text + '</span>';
+                }
+                else {
+                    li.innerHTML =
+                        '<span>' + task.text + '</span>' +
+                        '<div>' +
+                        '<button class="complete-btn" onclick="completeTask(' + task.id + ')">' +
+                        'Completar </button>' +
+                        '<button class="edit-btn" onclick="editTask(' + task.id + ')">' +
+                        'Editar </button>' +
+                        '<button class="delete-btn" onclick="deleteTask(' + task.id + ')">' +
+                        'Eliminar </button>' +
+                        '</div>';
+                }
                 taskList.appendChild(li);
             }
 
@@ -71,4 +125,13 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    window.completeTask = function (id) {
+        tasks = tasks.map(task =>
+            task.id === id ? {
+                ...task, complete: true
+            } : task);
+        renderTasks();
+    }
+
+    renderTasks();
 });
